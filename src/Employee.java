9b4c0b18f67bef9803a82647bf2624fa093fb251
Nourvin Mohammad Dabs <nourvin578@gmail.com>
2026-05-28 15:19:54 +0200

    Add Employee class and employee information logic

---
+++ src/Employee.java
@@ -0,0 +1,37 @@
+public class Employee {
+
+    private int employeeId;
+    private String employeeName;
+    private String department;
+    private double salary;
+
+    public Employee(int employeeId, String employeeName, String department, double salary) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public void displayEmployeeInfo() {
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Employee Name: " + employeeName);
+        System.out.println("Department: " + department);
+        System.out.println("Salary: €" + salary);
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
